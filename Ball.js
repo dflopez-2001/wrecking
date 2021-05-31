f6c521cd6d650371601ad00c2243bdf739f1178d
dflopez-2001 <83239928+dflopez-2001@users.noreply.github.com>
2021-05-31 10:56:35 -0700

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,20 @@
+class Ball {
+    constructor(x,y,width,height) {
+      var options = {
+          'frictionAir':0.01,
+          'density':1
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("lime");
+      ellipse(pos.x,pos.y, this.width, this.height);
+      
+    }
+  };
+  
